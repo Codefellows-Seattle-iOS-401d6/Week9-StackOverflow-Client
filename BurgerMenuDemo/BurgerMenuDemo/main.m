794bb1cb1559ecea95228d548c834f258ac8d7b5
Derek Graham <derekgraham@mac.com>
2016-08-01 22:34:15 -0700

    burger menu with keychain and nserror

---
+++ BurgerMenuDemo/BurgerMenuDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BurgerMenuDemo
+//
+//  Created by Derek Graham on 8/1/16.
+//  Copyright © 2016 Derek Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ BurgerMenuDemo/BurgerMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  BurgerMenuViewController.h
+//  BurgerMenuDemo
+//
+//  Created by Derek Graham on 8/1/16.
+//  Copyright © 2016 Derek Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BurgerMenuViewController : UIViewController
+
+@end
