a4df5be4d2fbadf60fd38a55d6890a8ab840bc1c
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-08-01 22:22:59 -0700

    monday lab without keychain. sad face.

---
+++ stackOverflowClient/stackOverflowClient/BurgerMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  BurgerMenuViewController.h
+//  stackOverflowClient
+//
+//  Created by Erin Roby on 8/1/16.
+//  Copyright © 2016 Erin Roby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BurgerMenuViewController : UIViewController
+
+@end
