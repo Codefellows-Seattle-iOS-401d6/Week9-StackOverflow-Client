b1315e64f6e7af524854140631580707c84fcfa5
hannah gaskins <hannahgaskins@hannahs-MacBook-Pro.local>
2016-08-01 17:57:53 -0700

    adding project with keychain and oauth

---
+++ hannahGaskins/StackOverFlowClient/StackOverFlowClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverFlowClient
+//
+//  Created by hannah gaskins on 8/1/16.
+//  Copyright © 2016 hannah gaskins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hannahGaskins/StackOverFlowClient/StackOverFlowClient/WebOAuthViewController.h
@@ -0,0 +1,15 @@
+//
+//  WebOAuthViewController.h
+//  StackOverFlowClient
+//
+//  Created by hannah gaskins on 8/1/16.
+//  Copyright © 2016 hannah gaskins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebOAuthViewController : UIViewController
+
+- (NSString *)accessToken;
+
+@end
